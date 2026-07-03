88adf90cb79adf355878cef27a127adc863b9ebd
Ajay Kumar (ajAI) <66588767+sapajaykumar@users.noreply.github.com>
2026-07-03 20:27:24 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
-
 """
-IFDSF Main module
+IFDSF Prototype v1.0
+
+Main Entry Point
 """
 
-print("main.py created successfully")
+from src.config import *
+
+
+def main():
+
+    print("=" * 60)
+    print(PROJECT_NAME)
+    print(VERSION)
+    print("=" * 60)
+
+    print(f"Project Root : {PROJECT_ROOT}")
+    print(f"Data Folder  : {DATA_DIR}")
+    print(f"Random Seed  : {RANDOM_SEED}")
+    print(f"Forecast AI  : {FORECAST_MODEL}")
+    print(f"RL Algorithm : {RL_ALGORITHM}")
+
+    print("\nIFDSF Prototype Successfully Started")
+
+
+if __name__ == "__main__":
+    main()
